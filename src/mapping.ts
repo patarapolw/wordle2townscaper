f33be84815a3c70f960339b092fb6fdc2a560754
Pacharapol Withayasakpunt <polv@polv.cc>
2022-02-28 09:43:44 +0700

    add more emoji support

--- src/mapping.ts
+++ src/mapping.ts
@@ -7,7 +7,11 @@ export const similar = {
   '🟨': ['🟡', ':yellow_square:', ':yellow_circle:'],
   '🟩': ['🟢', ':green_square:', ':green_circle:'],
   '🟦': [':blue_square:', ':large_blue_square:'],
-  '🟪': [':purple_square:', ':large_purple_square:']
+  '🟪': [':purple_square:', ':large_purple_square:'],
+  '🔼': [':arrow_up_small:', ':arrow_up:'],
+  '↕️': [':arrow_up_down:'],
+  '↔': ['↔ ', ':arrow_left_right:'],
+  '🔽': [':arrow_down_small:', ':arrow_down:']
 }
 
 export const mapping: Record<keyof typeof similar, number> = {
@@ -17,8 +21,12 @@ export const mapping: Record<keyof typeof similar, number> = {
   '🟥': 1,
   '🟧': 2,
   '🟨': 3,
+  '🔽': 3,
+  '↔': 3,
   '🟩': 5,
   '🟦': 10,
+  '🔼': 10,
+  '↕️': 10,
   '🟪': 11
 }
 

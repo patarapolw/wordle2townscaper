20a0cd3e425580eec0836f7364c9e6f038875977
Pacharapol Withayasakpunt <polv@polv.cc>
2022-03-12 09:57:17 +0700

    add more icon support

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,42 +1,80 @@
 export const similar = {
   ' ': [],
   '⬜': [
-    '⚪️',
+    ':white_square:',
     ':white_large_square:',
+    '⚪️',
     ':white_circle:',
+    ':large_white_circle:',
     '🤍',
     ':white_heart:'
   ],
   '⬛': [
-    '⚫️',
+    ':black_square:',
     ':black_large_square:',
+    '⚫️',
     ':black_circle:',
+    ':large_black_circle:',
     '🖤',
     ':black_heart:'
   ],
-  '🟥': [':red_square:', ':large_red_square:', '❤️', ':red_heart:'],
-  '🟧': [':orange_square:', ':large_orange_square:', '🧡', ':orange_heart:'],
+  '🟥': [
+    ':red_square:',
+    ':large_red_square:',
+    '🔴',
+    ':red_circle:',
+    ':large_red_circle:',
+    '❤️',
+    ':red_heart:'
+  ],
+  '🟧': [
+    ':orange_square:',
+    ':large_orange_square:',
+    '🟠',
+    ':orange_circle:',
+    ':large_orange_circle:',
+    '🧡',
+    ':orange_heart:'
+  ],
   '🟨': [
-    '🟡',
     ':yellow_square:',
     ':large_yellow_square:',
+    '🟡',
     ':yellow_circle:',
+    ':large_yellow_circle:',
     '💛',
     ':yellow_heart:'
   ],
   '🟩': [
-    '🟢',
     ':green_square:',
     ':large_green_square:',
+    '🟢',
     ':green_circle:',
+    ':large_green_circle:',
     '💚',
     ':green_heart:'
   ],
-  '🟦': [':blue_square:', ':large_blue_square:', '💙', ':blue_heart:'],
-  '🟪': [':purple_square:', ':large_purple_square:', '💜', ':purple_heart:'],
+  '🟦': [
+    ':blue_square:',
+    ':large_blue_square:',
+    '🔵',
+    ':blue_circle:',
+    ':large_blue_circle:',
+    '💙',
+    ':blue_heart:'
+  ],
+  '🟪': [
+    ':purple_square:',
+    ':large_purple_square:',
+    '🟣',
+    ':purple_circle:',
+    ':large_purple_circle:',
+    '💜',
+    ':purple_heart:'
+  ],
   '🔼': [':arrow_up_small:', ':arrow_up:'],
   '↕️': [':arrow_up_down:'],
-  '↔': ['↔ ', ':arrow_left_right:'],
+  '↔': [':arrow_left_right:'],
   '🔽': [':arrow_down_small:', ':arrow_down:']
 }
 
